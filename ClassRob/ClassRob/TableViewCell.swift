d2bec9671b863fc6dd48b098589c4a8829316a6a
Paizhuo.Chen <lcdtyph@gmail.com>
2017-03-16 11:32:34 +0800

    add table view for news

---
+++ ClassRob/ClassRob/TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  ClassRob
+//
+//  Created by lcdtyph on 3/16/17.
+//  Copyright © 2017 lcdtyph. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    @IBOutlet weak var newsTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
