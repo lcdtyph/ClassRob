a59d95d5f014cf6fed0275acaa5212af06d64139
Paizhuo.Chen <lcdtyph@gmail.com>
2017-04-21 10:40:20 +0800

    add picker view

--- ClassRob/ClassRob/SearchViewController.swift
+++ ClassRob/ClassRob/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SearchViewController.swift
 //  ClassRob
 //
 //  Created by lcdtyph on 3/15/17.
@@ -9,14 +9,21 @@
 import UIKit
 import FMDB
 
-class FirstViewController: UIViewController {
+class SearchViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     var database: FMDatabase? = nil
+    let days = ["未指定", "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+    let times = ["未指定", "第1节-第2节", "第3节-第4节", "第5节-第6节", "第7节-第8节", "第9节-第10节", "第11节-第12节", "第13节-第14节"]
+    var selectedDay: Int
+    var selectedTime: Int
+
+    @IBOutlet weak var pickerView: UIPickerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        pickerView.delegate = self
+        pickerView.dataSource = self
         initDatabase()
     }
 
@@ -55,5 +62,26 @@ class FirstViewController: UIViewController {
         self.database?.close()
         self.database = nil
     }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return component == 0 ? days.count : times.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return component == 0 ? days[row] : times[row];
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            selectedDay = row
+        } else if component == 1 {
+            selectedTime = row
+        }
+    }
+
 }
 
